d15030568af530937566af08dddeb779f041eac9
arnold <arnold@jasny.net>
2026-04-26 12:33:56 +0200

    Configure cucumber

--- cucumber.js
+++ cucumber.js
@@ -1,6 +1,27 @@
-export default {
-  require: ['features/support/**/*.js'],
-  worldParameters: {
+import { resolveDebugWorldParameters } from '@letsrunit/cucumber/config';
+
+const { failFast, worldParameters } = resolveDebugWorldParameters({
+  argv: process.argv,
+  baseWorldParameters: {
     baseURL: 'http://localhost:3000',
   },
+});
+
+const format = process.env['CODEX_CI'] ? '@letsrunit/cucumber/agent' : '@letsrunit/cucumber/progress';
+
+export default {
+  require: ['features/support/**/*.js'],
+  format: ['@letsrunit/cucumber/progress'],
+  failFast,
+  plugin: ['@letsrunit/cucumber/store'],
+  pluginOptions: {
+    letsrunitStore: {
+      directory: '.letsrunit',
+    },
+  },
+  worldParameters,
+  letsrunit: {
+    ignore: ['features/support/world.js'],
+  },
 };
+
